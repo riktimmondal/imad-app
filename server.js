1fd79f635ab506c4f5f029193b859c5c9649a9bd
Riktim Mondal <riktimrules@gmail.com>
2017-08-02 16:35:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendFile(path.join(__dirname, 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.sendFile(path.join(__dirname, 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+ res.sendFile(path.join(__dirname, 'article-three.html'));
     
 });
 // Do not change port, otherwise your app won't run on IMAD servers
