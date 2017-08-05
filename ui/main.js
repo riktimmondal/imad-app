858a5b4270133583e3d9aa682e54511859740557
Riktim Mondal <riktimrules@gmail.com>
2017-08-05 11:25:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,12 +17,29 @@
 
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+// var counter=0;
 button.onClick=function()
 {
+    //Create request object
+    var request =new XMLHTTPRequest();
+    //CAPTURE THE RESPOND AND STORE IT IN A VARIABLE
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHTTPRequest.DONE){
+            //TAKE SOME ACTION
+            if(request.status==200)//Cheching sucessful status
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        //IF NOT
+    };
+        //MAKE REQUEST 
+        request.open('GET','http://riktimrules.imad.hasura-app.io/',true);
+        request.send(null);
     
+    //RENDER THE VARIABLE IN CORRECT SPAN
+    // counter+=1;
     
-    counter+=1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
 }
