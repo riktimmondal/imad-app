acfb0630d639f10e26eac7ec097879e439264b13
Riktim Mondal <riktimrules@gmail.com>
2017-08-05 09:09:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //changing innerHTML using js
 var element=document.getElementById('main=text');
 element=innerHTML='New Value';
+
+//move the image
+var img=document.getElementById('madi');
+img.onClick=function(){
+  img.style.marginLeft='100px';  
+};
