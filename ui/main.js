f6f6d20091aae4680272b28ac05ad0632e1548d7
Riktim Mondal <riktimrules@gmail.com>
2017-08-08 13:28:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,5 +41,20 @@ button.onClick=function()
     
     //RENDER THE VARIABLE IN CORRECT SPAN
     // counter+=1;
-    
+}
+//Submit Nmae
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onClick = function(){
+    //Make a request to the server and send the name
+    //Capture a lsit of name as render as a list
+    var names=['name1','name2'.'name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getELementById('namelist');
+    ul.innerHTML-list;
 }
